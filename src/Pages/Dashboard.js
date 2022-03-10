cf494aca5c87b8ef788d969e72a2e07374fb6933
Franchesca <ihuoma.franchesca@gmail.com>
2022-03-10 16:46:19 +0100

    Edited screen

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,26 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import Aside from "../Components/Aside/Aside";
 import HomePage from "./HomePage";
-// import "./dashboard.css";
 
 const Dashboard = () => {
-  const [show, setShow] = useState(true);
   return (
     <div className="d-flex dashboard">
-      {/* <Aside /> */}
-      {show ? <Aside /> : null}
-      <button
-        onClick={() => setShow(!show)}
-        style={{
-          // width: 35,
-          height: 25,
-          border: "none",
-          background: "#017189",
-          color: "black",
-        }}
-      >
-        {/* Click */}
-      </button>
+      <Aside />
       <HomePage />
     </div>
   );
